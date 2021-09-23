7034510af85e62bfc2b3c34c20e16c45e66bcc8a
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-09-23 20:40:07 +0100

    skills styling

--- assests/js/main.js
+++ assests/js/main.js
@@ -32,3 +32,22 @@ function linkAction() {
 for (link of navLink) {
     link.addEventListener('click', linkAction);
 };
+
+/** ============ ACCORDIAN SKILLS */
+const skills = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills () {
+    let itemClass = this.parentNode.className; 
+
+    for (i=0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+}); 
