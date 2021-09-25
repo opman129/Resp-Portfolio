9540a2665c01b84e1883150cb2d968dde5231d95
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-09-25 22:49:20 +0100

    swiper

--- assests/js/main.js
+++ assests/js/main.js
@@ -98,12 +98,15 @@ modalCloses.forEach((modalClose) => {
 /** ===================== PORTFOLIO SWIPPER =================== */
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
+    loop: true,
+
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     mousewheel: true,
     keyboard: true,
