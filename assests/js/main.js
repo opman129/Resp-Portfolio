1aca02792e268612bc75633b709a850b6111834f
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-09-25 20:27:32 +0100

    servies css

--- assests/js/main.js
+++ assests/js/main.js
@@ -72,4 +72,12 @@ tabs.forEach(tab => {
     });
 });
 
+/** ================= SERVICES MODAL ================== */
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+};
 
