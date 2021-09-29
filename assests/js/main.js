dd3f74a6e4b954682acd5f2be60240a761cdc5bf
Jokotagba Opemipo <opemipojokotagba@gmail.com>
2021-09-29 10:18:47 +0100

    gni

--- assests/js/main.js
+++ assests/js/main.js
@@ -161,6 +161,8 @@ window.addEventListener('scroll', scrollActive)
 function scrollHeader(){
     const nav = document.getElementById('header')
     // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/** */
